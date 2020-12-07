d56ab147436c557861c9acc39b60f31613610144
Israel Felipe <israelsimplicio0@gmail.com>
2020-12-07 16:34:26 -0300

    Implementação dos componentes cadastro e login

--- wabisapi/wabis-ui/wabis/src/app/app.module.ts
+++ wabisapi/wabis-ui/wabis/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,8 @@ import { HomeComponent } from './home/home.component';
 import { SobrenosComponent } from './sobrenos/sobrenos.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +19,16 @@ import { FooterComponent } from './footer/footer.component';
     HomeComponent,
     SobrenosComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ wabisapi/wabis-ui/wabis/src/app/service/auth.service.ts
@@ -0,0 +1,44 @@
+import { UserLogin } from './../model/UserLogin';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../model/User';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  logar(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>('http://localhost:9000/usuarios/logar', userLogin)
+  }
+
+  cadastrar(user: User): Observable<User> {
+    return this.http.post<User>('http://localhost:9000/usuarios/cadastrar', user)
+  }
+
+  btnSair(){
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if (token != null) {
+      ok = true
+    }
+
+    return ok
+  }
+
+  btnLogin() {
+    let ok = false
+    let token = localStorage.getItem('token')
+
+    if (token == null) {
+      ok = true
+    }
+
+    return ok
+  }
+
+}
